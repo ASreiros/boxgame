2e4acdf972559d093aa6cb396c7993e0daae40d2
Anton Sokolkin <97021906+ASreiros@users.noreply.github.com>
2022-03-03 16:19:04 +0200

    generating balls

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+//Part below generates 2 boxes with windows
+
 const boxLeft = document.getElementById("left-box")
 let boxLefthtml = "";
 
@@ -17,3 +26,39 @@ boxRighthtml =boxRighthtml + temphtml;
 }
 
 boxRight.innerHTML = boxRighthtml;
+
+// part below generates balls with numbers
+setTimeout(ballgenerator,0)
+
+function ballgenerator() {
+//generuojam skirtingus 25 skaičius    
+const randomArr = [];    
+while (randomArr.length<25) {
+let checker = 0;    
+const element = rand(1,25);
+randomArr.forEach(a => {
+    if (a === element){
+checker++
+    }
+})
+
+if(checker === 0){
+randomArr.push(element)
+}
+}
+
+for (let i = 1; i < 26; i++) {
+const boxforball = document.querySelector(`#l${i}`);
+const ball = document.createElement(`p`);
+const ballnumber = document.createTextNode(`${randomArr[0]}`)
+ball.classList.add("ball")
+ball.style.color = "white" // tures buti invertinta spalva
+ball.style.backgroundColor = "green" //tures buti random
+
+
+
+ball.appendChild(ballnumber);
+boxforball.appendChild(ball);   
+}
+}
+
