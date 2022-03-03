099ffebf03d06862326d80d9d56f9d0c453a5bd6
Anton Sokolkin <97021906+ASreiros@users.noreply.github.com>
2022-03-03 15:09:41 +0200

    boxgame first commit

--- app.js
+++ app.js
@@ -7,3 +7,13 @@ boxLefthtml =boxLefthtml + temphtml;
 }
 
 boxLeft.innerHTML = boxLefthtml;
+
+const boxRight = document.getElementById("right-box")
+let boxRighthtml = "";
+
+for (let i = 1; i < 26; i++) {
+const temphtml = `<div class="smallboxR" id ="l${i}"></div>`
+boxRighthtml =boxRighthtml + temphtml;    
+}
+
+boxRight.innerHTML = boxRighthtml;
