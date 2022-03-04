a804c9ff27fc6e4a0db013db3151984119aa7c6d
Anton Sokolkin <97021906+ASreiros@users.noreply.github.com>
2022-03-04 21:34:21 +0200

    box game clock

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ function rand(min, max) {
 
 //Part below generates 2 boxes with windows
 
+let gamechecker = 1;
+
+let info = document.querySelector(".message-screen")
 const boxLeft = document.getElementById("left-box")
 let boxLefthtml = "";
 
@@ -21,7 +24,7 @@ const boxRight = document.getElementById("right-box")
 let boxRighthtml = "";
 
 for (let i = 1; i < 26; i++) {
-const temphtml = `<div class="smallboxR" id ="l${i}"></div>`
+const temphtml = `<div class="smallboxR" id ="r${i}"></div>`
 boxRighthtml =boxRighthtml + temphtml;    
 }
 
@@ -52,16 +55,58 @@ const boxforball = document.querySelector(`#l${i}`);
 const ball = document.createElement(`p`);
 const ballnumber = document.createTextNode(`${randomArr[0]}`)
 ball.classList.add("ball")
+ball.setAttribute("id", `bb${randomArr[0]}`);
+
+//spalvos blokas
 const color1 = rand(0,255);
 const color2 = rand(0,255);
 const color3 = rand(0,255);
-ball.style.color = `rgb(0, 0, 0)`
+let color4 = ""
+const suma = color1 + color2 + color3
+if (suma < 350) {
+color4 = "white"    
+} else{
+color4 = "black"    
+}
+ball.style.color = color4
 ball.style.backgroundColor = `rgb(${color1}, ${color2}, ${color3})` 
-
+//spalvos blokas-end
 
 randomArr.shift();
 ball.appendChild(ballnumber);
 boxforball.appendChild(ball);   
+ball.addEventListener("click", () => {
+    if(ball.id === `bb${gamechecker}`){
+    const house = document.getElementById(`r${gamechecker}`)
+    const iball = document.getElementById(`bb${gamechecker}`)
+    house.appendChild(iball)
+        gamechecker++;
+    } else {
+    const a = rand(1,3)
+    let atext = "";
+    switch (a) {
+        case 1:
+            atext = "Wrong ball"
+        break;
+        case 2:
+            atext = "Nope"
+        break;
+        case 3:
+            atext = "incorrect, click again"
+        break;
+    }    
+    info.innerHTML = atext;
+    setTimeout(textclear,1000)
+    function textclear() {
+        info.innerHTML = "";  
+    } 
+    }
+    
+    })
 }
 }
+
+
+const clock = document.querySelector("#clock")
+clock.innerHTML = "00:00:00"
 
