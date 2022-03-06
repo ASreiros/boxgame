3bc001ab91a460bc1ec0d3f0c1423354b70eaf31
Anton Sokolkin <97021906+ASreiros@users.noreply.github.com>
2022-03-06 02:16:01 +0200

    game main part done

--- app.js
+++ app.js
@@ -4,12 +4,42 @@ function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+const bbs = document.querySelector("#bbs");
+const bbc = document.querySelector("#bbc");
+let myTimeout = null;
+let info = document.querySelector(".message-screen")
 
+bbs.addEventListener("click", () => {
+    bbs.classList.add("bbhiden");
+    bbs.classList.remove("bbshown");
+    bbc.classList.remove("bbhiden");
+    bbc.classList.add("bbshown");
+    stopwatch();
+    ballgenerator()   
+})
+
+bbc.addEventListener("click", () => {
+    bbc.classList.add("bbhiden");
+    bbc.classList.remove("bbshown");
+    bbs.classList.remove("bbhiden");
+    bbs.classList.add("bbshown");
+    clearTimeout(myTimeout);
+    clock.innerHTML = "00:00:00" 
+    gamechecker = 1;
+    t = 1;
+    info.innerHTML = "Press start to play again"
+    for (let i = 1; i < 26; i++) {
+    document.querySelector(`#l${i}`).innerHTML = "";
+    document.querySelector(`#r${i}`).innerHTML = "";   
+    }
+    })
+
+ 
 //Part below generates 2 boxes with windows
 
 let gamechecker = 1;
 
-let info = document.querySelector(".message-screen")
+
 const boxLeft = document.getElementById("left-box")
 let boxLefthtml = "";
 
@@ -31,7 +61,7 @@ boxRighthtml =boxRighthtml + temphtml;
 boxRight.innerHTML = boxRighthtml;
 
 // part below generates balls with numbers
-setTimeout(ballgenerator,0)
+
 
 function ballgenerator() {
 //generuojam skirtingus 25 skaičius    
@@ -80,23 +110,35 @@ ball.addEventListener("click", () => {
     const house = document.getElementById(`r${gamechecker}`)
     const iball = document.getElementById(`bb${gamechecker}`)
     house.appendChild(iball)
-        gamechecker++;
+        if (gamechecker === 25){
+            info.innerHTML = "you won!!!"
+            clearTimeout(myTimeout);
+        }else{
+            gamechecker++;
+        }
     } else {
     const a = rand(1,3)
     let atext = "";
+    let textclearer = setTimeout(textclear,0);
     switch (a) {
         case 1:
-            atext = "Wrong ball"
+            atext = "Wrong ball";
+            clearTimeout(textclearer);
+            textclearer = setTimeout(textclear,1500);
         break;
         case 2:
             atext = "Nope"
+            clearTimeout(textclearer);
+            textclearer = setTimeout(textclear,1500);
         break;
         case 3:
             atext = "incorrect, click again"
+            clearTimeout(textclearer);
+            textclearer = setTimeout(textclear,1500);
         break;
     }    
     info.innerHTML = atext;
-    setTimeout(textclear,1000)
+    
     function textclear() {
         info.innerHTML = "";  
     } 
@@ -109,4 +151,16 @@ ball.addEventListener("click", () => {
 
 const clock = document.querySelector("#clock")
 clock.innerHTML = "00:00:00"
+let t = 0;
+
+function stopwatch() {   
+let t1 = Math.trunc(t/6000);
+let t2 = Math.trunc((t - t1*6000)/600)
+let t3 = Math.trunc((t - t1*6000 - t2*600)/100)
+let t4 = Math.trunc((t - - t1*6000 - t2*600 - t3*100)/10)
+let t5 = Math.trunc(t % 10)
+clock.innerHTML = `${t1}${t2}:${t3}${t4}:${t5}0`
+t++
+myTimeout = setTimeout(stopwatch, 100)   
+}
 
